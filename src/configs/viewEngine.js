8a18a6d2a3674cde511a80700940393e2cb0163b
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-04-17 16:41:09 +0700

    new public

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,6 +1,7 @@
-import React from "react";
+import express from "express";
 
 const viewEngine = (app) => {
+  app.use(express.static("./src/public"));
   app.set("view engine", "ejs");
   app.set("views", "./src/views");
 };
--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 import express from "express";
 import viewEngine from "./configs/viewEngine";
+require("dotenv").config();
+
 const app = express();
-const post = 4000;
+const post = process.env.PORT || 5000;
 
 viewEngine(app);
 
-// app.get("/", (req, res) => {
-//   res.send("hello world");
-// });
-
+// render view engine
 app.get("/", (req, res) => {
   res.render("test/index.ejs");
 });
